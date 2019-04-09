24cb399fd1c279c073224d08dc6ae273784cae8c
NikitaWas <theelderskrolls98@gmail.com>
2019-04-09 11:26:46 +0300

    First Full Stage

--- PythonGuide.py
+++ PythonGuide.py
@@ -4,3 +4,23 @@ print(2 ** 16)
 print(37 / 3)
 print(37 // 3)
 print(37 % 3)  
+
+
+print('Как вас зовут?')
+name = input()
+print('Здравствуйте, ' + name + '!')
+
+a = input()
+b = input()
+s = a + b
+print(s)
+
+first = 5
+second = 7
+print(first * second)
+
+
+d = int(input())
+e = int(input())
+f = d + e
+print(f)
