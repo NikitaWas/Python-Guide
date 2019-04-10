c42f4563abe38b30c0059ef1c9928a387b9ef2c6
NikitaWas <theelderskrolls98@gmail.com>
2019-04-10 13:27:58 +0300

    IFElse2

--- Python2.py
+++ Python2.py
@@ -2,10 +2,10 @@ print('Урок 2')
 print('Условия')
 print('Введите любое число')
 x = int(input())
-if x<0:
-    print(-x)
-else:
+if x>5:
     print(x)
+else:
+    print(5)
 
 x = int(input())
 if x < 0:
