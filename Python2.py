13c341194d49efd2ed6203968860cc1a60a8168c
NikitaWas <theelderskrolls98@gmail.com>
2019-04-10 14:58:00 +0300

    IFElse4

--- Python2.py
+++ Python2.py
@@ -24,3 +24,23 @@ else:
         print("Вторая четверть")
     else:                   # x < 0, y < 0
         print("Третья четверть")
+
+a = int(input())
+b = int(input())
+if a % 10 == 0 or b % 10 == 0:
+    print('YES')
+else:
+    print('NO')
+
+x = int(input())
+y = int(input())
+if x > 0 and y > 0:
+    print("Первая четверть")
+elif x > 0 and y < 0:
+    print("Четвертая четверть")
+elif y > 0:
+    print("Вторая четверть")
+else:
+    print("Третья четверть")
+
+print('Задача «Минимум из двух чисел»')
