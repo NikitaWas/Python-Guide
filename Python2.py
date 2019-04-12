0ad40942d7e937fa198aab58ea9999de23184b69
NikitaWas <theelderskrolls98@gmail.com>
2019-04-12 11:22:28 +0300

    Chod Konya

--- Python2.py
+++ Python2.py
@@ -185,3 +185,12 @@ else:
             print('NO')
     else:
         print('NO')
+
+print("Задача «Шоколадка»")
+n = int(input())
+m = int(input())
+k = int(input())
+if n*m >= k and (k % n == 0 or k % m == 0):
+    print('YES')
+else:
+    print('NO')
