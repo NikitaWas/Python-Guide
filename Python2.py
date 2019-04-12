662e919093f10bae2f4f44ef40698e227606fa55
NikitaWas <theelderskrolls98@gmail.com>
2019-04-12 10:18:44 +0300

    Chod Slona

--- Python2.py
+++ Python2.py
@@ -146,3 +146,13 @@ if (x2 - x1 <= 1 and x2 - x1 >= -1):
         print('NO')
 else:
     print('NO')
+
+print("Задача «Ход слона»")
+x1 = int(input())
+y1 = int(input())
+x2 = int(input())
+y2 = int(input())
+if abs(x1 - x2) == abs(y1 - y2):
+    print ("YES")
+else:
+    print ("NO")
