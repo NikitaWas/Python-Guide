f9059b11a216a4297a9cec13ef2c8730820e5b6a
NikitaWas <theelderskrolls98@gmail.com>
2019-04-12 10:51:24 +0300

    Chod Konya

--- Python2.py
+++ Python2.py
@@ -166,3 +166,22 @@ if abs(x1 - x2) == abs(y1 - y2) or x1 == x2 or y1 == y2:
     print('YES')
 else:
     print('NO')
+
+print("Задача «Ход коня»")
+x1 = int(input())
+y1 = int(input())
+x2 = int(input())
+y2 = int(input())
+if abs(x1 - x2) == 2:
+    if abs(y1 - y2) == 1:
+        print('YES')
+    else:
+        print('NO')
+else:
+    if abs(x1 - x2) == 1:
+        if abs(y1 - y2) == 2:
+            print('YES')
+        else:
+            print('NO')
+    else:
+        print('NO')
